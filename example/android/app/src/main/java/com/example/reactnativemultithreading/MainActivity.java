fa1dd9293e61b5bfda26bd5ecb4a9f8a41a3a279
osdnk <micosa97@gmail.com>
2021-10-05 23:36:12 +0300

    enable fabric

--- example/android/app/src/main/java/com/example/reactnativemultithreading/MainActivity.java
+++ example/android/app/src/main/java/com/example/reactnativemultithreading/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.reactnativemultithreading;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
 import com.facebook.react.config.ReactFeatureFlags;
 
 public class MainActivity extends ReactActivity {
@@ -8,6 +14,7 @@ public class MainActivity extends ReactActivity {
   static {
     ReactFeatureFlags.useTurboModules = true;
     ReactFeatureFlags.eagerInitializeFabric = true;
+    // ReactFeatureFlags.enableFabricRenderer = true;
   }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -17,4 +24,25 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "MultithreadingExample";
   }
+
+
+  // copied
+  public static class RNTesterActivityDelegate extends ReactActivityDelegate {
+    public RNTesterActivityDelegate(ReactActivity activity, @Nullable String mainComponentName) {
+      super(activity, mainComponentName);
+    }
+
+    @Override
+    protected ReactRootView createRootView() {
+      ReactRootView reactRootView = new ReactRootView(getContext());
+      reactRootView.setIsFabric(true);
+      return reactRootView;
+    }
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new RNTesterActivityDelegate(this, getMainComponentName());
+  }
+
 }
